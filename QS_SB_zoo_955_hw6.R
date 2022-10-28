aa90bd9b7323f7312adbea56408b093456f28e1a
quinnlanconnorsmith <quinnlanconnorsmith@gmail.com>
2022-10-28 10:17:26 -0500

    a simulation attempt?

--- QS_SB_zoo_955_hw6.R
+++ QS_SB_zoo_955_hw6.R
@@ -46,6 +46,71 @@ mlme1 <- lme(burrow_count ~ mean_burrow_diameter, random = ~1 | replicate,
              data = test_sim_data)
 summary(mlme1)
 
+
+##### Run simulation #####
+
+# Extract the values needed to create simulation data from the actual data
+
+burrow_count_mean <- mean(test_sim_data$burrow_count)
+burrow_count_sd <- sd(test_sim_data$burrow_count)
+replicates <- unique(test_sim_data$replicate)
+
+# Generate simulation data using random values from a distributions
+
+n <- 1000 # Number of values per simulation
+num_simulations <- 100
+
+# Prepare an empty data frmae to store simulation outputs
+df <- data.frame(matrix(ncol=1, nrow=num_simulations))
+names(df) <- c('sim_burrow_count')
+
+for(i in 1:nrow(df)) {
+  
+  # Create simulated data
+  simulated_data <- data.frame(
+    replicate_site = sample(replicates, n, replace=T), 
+    burrows = rnorm(n, mean=burrow_count_mean, sd = burrow_count_sd))
+  
+  # Generate model using simulation data
+  simulated_mod1 <- gls(burrows ~ 1 + replicate_site,
+                         method = "REML", data = simulated_data)
+  
+  # Extract p-value and store
+  df[i,1] <- summary(simulated_mod1)$tTable[2,4]
+}
+
+#Our data is way too low though (given observed burrow counts), is it bound by 1? 
+
+
+
+
+
+#### Frankenstein Code ####
+
+#Some baseline simulation of data (2 variables, maybe bail on this )
+x1=rnorm(1000,7.875,9.90)
+x2=rnorm(1000,22.23, 33.17)
+error=rnorm(1000,0,0.5)
+#Generate the dependent variable (b0=1, b1=2, b2=3)
+y1=beta0+(beta1*x1)+(beta2*x2)+error
+#create the model
+m1=lm(y1~x1+x2)
+summary(m1)
+plot(m1)
+
+
+df<- data.frame(matrix(ncol=2, nrow=1000))
+colnames(df)<- c("bur_count","bur_diameter")
+
+for(i in 1:nrow(df)){
+  #run function over 5 n levels and store probs for two model types as a list
+  list<- (df$n[i])
+  #extract first probability in list
+  df$bur_count[i]<- as.numeric(unlist(list[1]))
+  #extract second probability in list
+ df$bur_diameter[i]<- as.numeric(unlist(list[2]))
+}
+
 #Simulating the data - the actual homework 
 #To make things easier let's just focus on burrow count 
 summary(test_sim_data2)
@@ -79,30 +144,3 @@ summary(bur_count5)
 #set regression coefficients
 beta0 <- 1
 beta1 <- 2
-
-
-#Some baseline simulation of data 
-x1=rnorm(1000,7.875,9.90)
-x2=rnorm(1000,22.23, 33.17)
-error=rnorm(1000,0,0.5)
-#Generate the dependent variable (b0=1, b1=2, b2=3)
-y1=beta0+(beta1*x1)+(beta2*x2)+error
-#create the model
-m1=lm(y1~x1+x2)
-summary(m1)
-plot(m1)
-
-
-df<- data.frame(matrix(ncol=2, nrow=1000))
-colnames(df)<- c("bur_count","bur_diameter")
-
-for(i in 1:nrow(df)){
-  #run function over 5 n levels and store probs for two model types as a list
-  list<- (df$n[i])
-  #extract first probability in list
-  df$bur_count[i]<- as.numeric(unlist(list[1]))
-  #extract second probability in list
- df$bur_diameter[i]<- as.numeric(unlist(list[2]))
-}
-
-
